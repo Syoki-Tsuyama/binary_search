e377977b7c5d0d09f7339fa022762c2fb5451e68
opfer <OPF-10155@outlook.jp>
2019-06-19 04:08:02 +0900

    complete spear.c

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,39 @@ int n;
 int k;
 int A[100000];
 
+// 小数点以下を全て切り上げる割り算
+int Div(unsigned int x, unsigned int y) {
+  if (x % y == 0) return x / y;
+  else return (x / y) + 1;
+}
+
+// boolへの関数pが単調減少(p(x) <= p(x-1))のとき、p(x)==1 なる最大のxを調べる
+int binarySearch_P (int (*p)(int,int), int left, int right) {
+  if (right <= left) return left;
+  
+  int mid = Div(left+right , 2);
+  if (p(mid, k)) return binarySearch_P(p, mid, right);
+  else return binarySearch_P(p, left, mid - 1);
+}
+
+// 長さlenの槍をspearNum本作れるか調べる
+int canMakeSpears(int len, int spearNum) {
+  if (len <= 0) return 0;
+
+  int tmp = spearNum;
+  for (int i=0; i < n; i++) {
+    tmp -= A[i] / len;
+    if (tmp <= 0) return 1;
+  }
+  return 0;
+}
+
 
 int main(){
-  int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  for(int i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  printf("%d\n", binarySearch_P(canMakeSpears, 0, A[n-1]));
   return 0;
 }

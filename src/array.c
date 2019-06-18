d8b4fe283b38204733a4e53149a5eb703f1260fc
opfer <OPF-10155@outlook.jp>
2019-06-18 15:52:08 +0900

    complete array.c

--- src/array.c
+++ src/array.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int n;
-int k;
-int A[100000];
+int a[100000];
 
+int binarySearch(int arr[], int target, int left, int right) {
+  if (left >= right) return right;
 
-int main(){
-  int i, lb, ub;
+  int mid = (right + left) / 2;
+  if (arr[mid] < target) return binarySearch(arr, target, mid+1, right);
+  else return binarySearch(arr, target, left, mid);
+  return 0;
+}
+
+int main (void) {
+  int n, k;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
+  for(int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
   }
 
-
-
+  printf("%d\n", binarySearch(a, k, 0, n));
   return 0;
 }
